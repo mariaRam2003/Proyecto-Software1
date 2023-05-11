cdde84941b28d0a41a41c1a6d0f4c4b459a6adf1
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-10 19:27:16 -0600

    Tabla en componente ANICAM

--- src/pages/ANICAM.js
+++ src/pages/ANICAM.js
@@ -1,7 +1,31 @@
+import supabase from "../config/supabaseClient"
+import BasicTable from "../components/BasicTable"
+import { testQuery } from "../config/supabaseClient"
+import { useEffect, useState } from "react"
+
+
 const ANICAM = () => {
+
+  const [data, setData] = useState(null)
+  //console.log(supabase) //borrar
+
+  useEffect(() => {
+    testQuery().then(data => {
+      setData(data)      
+    })
+    
+  }, [])
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <div className="page anicam">
       <h2>ANICAM</h2>
+      <BasicTable data = {data}/>  
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,14 @@
-import supabase from "../config/supabaseClient"
-import BasicTable from "../components/BasicTable"
-import { testQuery } from "../config/supabaseClient"
-import { useEffect, useState } from "react"
+
 
 const Home = () => {
-  const [data, setData] = useState(null)
-  //console.log(supabase) //borrar
-
-  useEffect(() => {
-    testQuery().then(data => {
-      setData(data)      
-    })
-    
-  }, [])
-
-  useEffect(() => {
-    if (data && data.length > 0) {
-      console.log(data);
-    }
-  }, [data]);
+  
     
   
 
   return (
     <div className="page home">
       <h2>Home</h2>
-      <BasicTable data = {data}/>      
+          
       <p></p>
     </div>
   )
