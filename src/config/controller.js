712fcf9ddaa2c0ed9bfd796af053c79997f7fa75
gusanitor8 <gusanitor8@gmail.com>
2023-06-02 16:39:32 -0600

    commit listo

--- src/config/controller.js
+++ src/config/controller.js
@@ -1,10 +1,24 @@
 import { insertAnicamViewData } from "./supabaseClient";
 
+const updateArray = (array) => {
+    const updatedArray = array.map(obj => {
+            // Create a new object with modified key names
+            const updatedObj = {};
+            for (let key in obj) {
+                updatedObj['p_' + key] = obj[key];
+            }
+            return updatedObj;
+        });
+    console.log('updatedArray', updatedArray)
+    return updatedArray;
+};
+
 const insertData = async (data) => {
-  data.map((row) => {
-    console.log("row", row);
-    insertAnicamViewData(data);
-  });
+    const updatedData = updateArray(data);
+
+    updatedData.map((row) => {        
+        insertAnicamViewData(row);
+    });
 };
 
 export { insertData };
