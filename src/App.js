f10dedd2c1ec3e803a5c1f6a848bbb607e5dfbef
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-09-03 00:27:51 -0600

    UI fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// Style
+import Backdrop from "@mui/material/Backdrop";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FlightTakeoffTwoToneIcon from "@mui/icons-material/FlightTakeoffTwoTone";
+import "./index.css";
 
 // Importar las páginas principales
 import VIEWS from "./pages/principalPages/Views";
@@ -16,20 +24,39 @@ import FINANCE from "./pages/viewsPages/FINANCE";
  * Componente raíz que define las rutas y la navegación de la aplicación.
  */
 function App() {
+  // Estado para controlar si el menú aside está abierto
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Función para abrir el menú aside
+  const openMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  // Función para cerrar el menú aside
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <BrowserRouter>
       {/* Navegación */}
       <nav>
-        <Link to="/">
-          <img
-            src="https://lirp.cdn-website.com/1b29e013/dms3rep/multi/opt/GRUPO-SLI-01-151w.png"
-            alt="Logo-Grupo-SLI"
-          />
-        </Link>
+        <FlightTakeoffTwoToneIcon
+          sx={{
+            fontSize: 50,
+            paddingLeft: 3,
+            paddingRight: 3,
+            paddingTop: 1,
+            paddingBottom: 1,
+          }}
+        />
         <Link to="/">VIEWS</Link>
         <Link to="/CUSCAR">CUSCAR</Link>
         <Link to="/validacion">VALIDACION</Link>
         <Link to="/facturacion">FACTURACION</Link>
+        <button onClick={openMenu}>
+          <AccountCircleIcon sx={{ fontSize: 45 }} />
+        </button>
       </nav>
 
       {/* Definición de rutas */}
@@ -43,6 +70,17 @@ function App() {
         <Route path="/OPERATIONS" element={<OPERATIONS />} />
         <Route path="/FINANCE" element={<FINANCE />} />
       </Routes>
+
+      <aside className={isMenuOpen ? "menu-open" : "menu-closed"}>
+        {/* Contenido del menú aside */}
+        <ul>
+          <AccountCircleIcon sx={{ fontSize: 80, paddingLeft: 7 }} />
+          <li>Perfil</li>
+          <li>Cerrar Sesion</li>
+        </ul>
+        {/* Botón para cerrar el menú aside */}
+        <button onClick={closeMenu}>Cerrar</button>
+      </aside>
     </BrowserRouter>
   );
 }
